c5a308ed6fcf46078a1edcdb36771d3f7814798e
TNLHK <81743463+TNLHK@users.noreply.github.com>
2022-11-21 19:53:05 +0800

    Update 发送作死报（测试版本）.js

--- 发送作死报（测试版本）.js
+++ 发送作死报（测试版本）.js
@@ -18,8 +18,8 @@ bot
 					console.log(`第${i+1}个页面：${result0.query.pages[0].links[i].title}。`);
 					const result1 = await bot.doEdit({
 						title: result0.query.pages[0].links[i].title,
-						appendtext: '\n{{safesubst:U:TNLHK/萌百作死报}}<span style="display:none;">{{mute|{{safesubst:ROOTPAGENAME}}}} 无情的期刊发送小帮手~~~</span>',
-						summary: '作死小帮手：《[[U:TNLHK/萌百作死报|作死报]]》第2022-05期已送达，请注意查收~',
+						appendtext: '\n{{safesubst:U:TNLHK/萌百作死报}}<span style="display:none;">{{mute|{{safesubst:ROOTPAGENAME}}}} ，您定的作死报已送达，请查收！~~~</span>',
+						summary: '新的一期《[[U:TNLHK/萌百作死报|作死报]]》已送达，请注意查收~',
 						tags: 'Bot',
 						Bot: true,
 					});
